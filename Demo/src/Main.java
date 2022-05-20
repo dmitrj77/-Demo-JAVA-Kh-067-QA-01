50fda4ae5af480f2434390c6e5cd652f33726817
Daniel Dmitry <dmitrj77@gmail.com>
2022-05-20 12:23:56 +0300

    modified demo

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -1,17 +1,2 @@
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class Main{
-    public static void main(string[] args, Stage Stage){
-
-        Application application = new Application() {
-            @Override
-            public void start(Stage primaryStage) throws Exception {
-
-            }
-        };
-
-        application.start(Stage);
-
-    }
+public class Main {
 }
