531e7aa309120006251c408981b80b9440c626a3
Daniel Dmitry <dmitrj77@gmail.com>
2022-05-20 07:59:58 +0300

    created Demo

---
+++ Demo/Demo/src/Application/Main.java
@@ -0,0 +1,10 @@
+package Application;
+
+public class Main {
+    public static void main(string[] args){
+
+        Application application = new Application();
+        application.run();
+
+    }
+}
---
+++ Demo/src/Main.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main{
+    public static void main(string[] args, Stage Stage){
+
+        Application application = new Application() {
+            @Override
+            public void start(Stage primaryStage) throws Exception {
+
+            }
+        };
+
+        application.start(Stage);
+
+    }
+}
