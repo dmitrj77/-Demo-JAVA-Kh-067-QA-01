2edeb78c16f0e59e860a7215da7eb6876cef044e
Daniel Dmitry <dmitrj77@gmail.com>
2022-05-23 20:00:07 +0300

    rename variables and added getMenu method

--- src/App.java
+++ src/App.java
@@ -1,76 +1,46 @@
 import javafx.concurrent.Task;
 
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
     static void runPrecondition() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("\n[Organizer] is designed to schedule and schedule user activity. " +
-                "\nThe program allows you to create, edit, view, delete and restore tasks." +
-                "\nThe program has a console implementation." +
-                "\nTo control the application, " + "select a number from the menu and then follow the prompts." +
-                "\nThe program was created by the team KH-JAVA-067-QA-01." +
-                "\nThe team is the copyright holder of this program.");
-        System.out.println("");
-    }
-
-    static void run() {
-        Scanner sc = new Scanner(System.in);
-        final String taskFile = null;
-        final String deletedFile = null;
-
         // Call method createIfNotExists
         // Call method readFile from tasks
         // Call method readFile from deletedTasks
         List<Task> tasks = new ArrayList<>();
         List<Task> deletedTasks = new ArrayList<>();
-        int operator;
+        System.out.println("\n[Organizer] is designed to schedule and schedule user activity. " +
+                "\nThe program allows you to create, edit, view, delete and restore tasks." +
+                "\nThe program has a console implementation." +
+                "\nTo control the application, select a number from the menu and then follow the prompts." +
+                "\nThe program was created by the team KH-JAVA-067-QA-01." +
+                "\nThe team is the copyright holder of this program.");
+    }
 
-        int command = 9;
-        while (command != 0) {
-            System.out.println("====ORGANIZER====");
-            System.out.println("----Main menu----");
-            System.out.println("Add task      [1]");
-            System.out.println("Edit task     [2]");
-            System.out.println("Show task     [3]");
-            System.out.println("Delete task   [4]");
-            System.out.println("Quit          [0]");
-            try {
-                operator = getNumber(4);
-            } catch (NotSupportedOperationException e) {
-                System.out.println("Exception:" + e);
-                operator = Integer.parseInt(sc.next());
-            }
-            switch (operator) {
+    static void run() {
+        Scanner scanner = new Scanner(System.in);
+        final String taskFile = null;
+        final String deletedFile = null;
+        int userChoice;
+        String comm = "";
+        String command = "";
+        while (comm != "exit") {
+            String[] menu = new String[]{"MAIN MENU","Add task","Edit task","Show task","Delete task","Quit"};
+            getMenu(menu);
+            userChoice = getUserChoice(4);
+            switch (userChoice) {
                 case 1: {
-                    int command1 = 9;
-                    while (command1 != 0) {
-                        System.out.println("-----Add task----");
-                        System.out.println("By steps      [1]");
-                        System.out.println("By pattern    [2]");
-                        System.out.println("Back          [0]");
-                        try {
-                            operator = getNumber(2);
-                        } catch (NotSupportedOperationException e) {
-                            System.out.println("Exception:" + e);
-                            operator = Integer.parseInt(sc.next());
-                        }
-                        switch (operator) {
+                    command = "";
+                    while (command != "exit") {
+                        menu = new String[]{"ADD TASK","By steps","By pattern","Back"};
+                        getMenu(menu);
+                        userChoice = getUserChoice(2);
+                        switch (userChoice) {
                             case 1: {
-                                System.out.println("-----By steps----");
-                                System.out.println("Continue      [1]");
-                                System.out.println("Cancel        [0]");
-                                try {
-
-                                    operator = getNumber(1);
-                                } catch (NotSupportedOperationException e) {
-                                    System.out.println("Exception:" + e);
-                                    operator = Integer.parseInt(sc.next());
-                                }
-                                if (operator == 1) {
+                                menu = new String[]{"BY STEPS","Continue","Cancel"};
+                                getMenu(menu);
+                                userChoice = getUserChoice(1);
+                                if (userChoice == 1) {
                                     //Call method writeFile with 2 parameters " +
                                     // "(filePath and List<String> to write)
                                     System.out.println("Call method writeFile with 2 parameters " +
@@ -80,17 +50,10 @@ public class App {
                             }
                             break;
                             case 2: {
-                                System.out.println("----By pattern---");
-                                System.out.println("Continue      [1]");
-                                System.out.println("Cancel        [0]");
-                                try {
-
-                                    operator = getNumber(1);
-                                } catch (NotSupportedOperationException e) {
-                                    System.out.println("Exception:" + e);
-                                    operator = Integer.parseInt(sc.next());
-                                }
-                                if (operator == 1) {
+                                menu = new String[]{"BY PATTERN","Continue","Cancel"};
+                                getMenu(menu);
+                                userChoice = getUserChoice(1);
+                                if (userChoice == 1) {
                                     //Call method writeFile with 2 parameters " +
                                     // "(filePath and List<String> to write)
                                     System.out.println("Call method writeFile with 2 parameters " +
@@ -100,112 +63,108 @@ public class App {
                             }
                             break;
                             case 0: {
-                                command1 = 0;
+                                command = "exit";
                                 break;
                             }
+                            default: {
+                                System.out.println("Please make your choice");
+                            }
+                            break;
                         }
                     }
                 }
                 break;
                 case 2: {
-                    int command2 = 9;
-                    while (command2 != 0) {
-                        System.out.println("-----Edit task---");
-                        System.out.println("Existing      [1]");
-                        System.out.println("Recycle Bin   [2]");
-                        System.out.println("Back          [0]");
-                        try {
-                            // String[] chars = new String[]{"1", "2", "0"};
-                            operator = getNumber(2);
-                        } catch (NotSupportedOperationException e) {
-                            System.out.println("Exception:" + e);
-                            operator = Integer.parseInt(sc.next());
-                        }
-                        switch (operator) {
+                    command = "";
+                    while (command != "exit") {
+                        menu = new String[]{"EDIT TASK","Existing ","Recycle Bin","Back"};
+                        getMenu(menu);
+                        userChoice = getUserChoice(2);
+                        switch (userChoice) {
                             case 1: {
-                                System.out.println("--Existing--");
+                                menu = new String[]{"EXISTING"};
+                                getMenu(menu);
                                 //Call method readFile from List<Task> tasks = new ArrayList<>();
                                 System.out.println("Call method readFile from List<Task> " +
                                         "tasks = new ArrayList<>()");
                                 System.out.println("Call method writeFile with 2 parameters " +
                                         "(filePath and List<String> to write)  ");
-                                break;
+
                             }
+                            break;
                             case 2: {
-                                System.out.println("-Recycle Bin-");
+                                menu = new String[]{"RECYCLE BIN"};
+                                getMenu(menu);
                                 //Call method readFile from List<Task> deletedTasks = new ArrayList<>();
                                 System.out.println("Call method readFile from List<Task> " +
                                         "deletedTasks = new ArrayList<>();");
                                 System.out.println("Call method writeFile with 2 parameters " +
                                         "(filePath and List<String> to write)  ");
-                                break;
                             }
+                            break;
                             case 0: {
-                                command2 = 0;
-                                break;
+                                command = "exit";
+                            }
+                            break;
+                            default: {
+                                System.out.println("Please make your choice");
                             }
                         }
                     }
                 }
                 break;
                 case 3: {
-                    int command3 = 9;
-                    while (command3 != 0) {
-                        System.out.println("-----Showing-----");
-                        System.out.println("All tasks     [1]");
-                        System.out.println("By filter     [2]");
-                        System.out.println("Deleted       [3]");
-                        System.out.println("Back          [0]");
-                        try {
-                            operator = getNumber(3);
-                        } catch (NotSupportedOperationException e) {
-                            System.out.println("Exception:" + e);
-                            operator = Integer.parseInt(sc.next());
-                        }
-                        switch (operator) {
+                    command = "";
+                    while (command != "exit") {
+                        menu = new String[]{"SHOWING","All tasks", "By filter", "Deleted", "Back"};
+                        getMenu(menu);
+                        userChoice = getUserChoice(3);
+                        switch (userChoice) {
                             case 1: {
-                                System.out.println("--All tasks--");
+                                menu = new String[]{"ALL TASK"};
+                                getMenu(menu);
                                 System.out.println("Call method readFile from List<Task> " +
                                         "tasks = new ArrayList<>()");
-                                break;
                             }
+                            break;
                             case 2: {
-                                System.out.println("--By filter--");
+                                menu = new String[]{"BY FILTER"};
+                                getMenu(menu);
                                 System.out.println("Call method readFileByFilter from List<Task> " +
                                         "tasks = new ArrayList<>()");
-                                break;
                             }
+                            break;
                             case 3: {
-                                System.out.println("---Deleted---");
+                                menu = new String[]{"DELETE"};
+                                getMenu(menu);
                                 //Call method readFile from List<Task> deletedTasks = new ArrayList<>();
                                 System.out.println("Call method readFile from List<Task> " +
                                         "deletedTasks = new ArrayList<>();");
-                                break;
                             }
+                            break;
+
                             case 0: {
-                                command3 = 0;
-                                break;
+                                command = "exit";
+
+                            }
+                            break;
+                            default: {
+                                System.out.println("Please make your choice");
                             }
                         }
                     }
                 }
                 break;
                 case 4: {
-                    int command4 = 9;
-                    while (command4 != 0) {
-                        System.out.println("------Delete-----");
-                        System.out.println("By Id         [1]");
-                        System.out.println("By period     [2]");
-                        System.out.println("Back          [0]");
-                        try {
-                            operator = getNumber(2);
-                        } catch (NotSupportedOperationException e) {
-                            System.out.println("Exception:" + e);
-                            operator = Integer.parseInt(sc.next());
-                        }
-                        switch (operator) {
+                    command = "";
+                    while (command != "exit") {
+                        menu = new String[]{"DELETE","By Id","By period","Back"};
+                        getMenu(menu);
+                        userChoice = getUserChoice(2);
+                        switch (userChoice) {
                             case 1: {
-                                System.out.println("----By Id----");
+                                menu = new String[]{"BY ID"};
+                                getMenu(menu);
                                 System.out.println("Call method readFile from List<Task> " +
                                         "tasks = new ArrayList<>()");
                                 System.out.println("Call method deletedTaskById from List<Task> " +
@@ -213,68 +172,96 @@ public class App {
                                 break;
                             }
                             case 2: {
-                                System.out.println("--By period--");
+                                menu = new String[]{"BY PERIOD"};
+                                getMenu(menu);
                                 System.out.println("Call method readFile from List<Task> " +
                                         "tasks = new ArrayList<>()");
                                 System.out.println("Call method deletedTaskByPeriod from List<Task> " +
                                         "tasks = new ArrayList<>()");
                                 break;
                             }
+
                             case 0: {
-                                command4 = 0;
+                                command = "exit";
                                 break;
                             }
                         }
                     }
                 }
                 break;
-                case 0: {
-                    command = 0;
-                    break;
+
+                default: {
+                    System.out.println("Please make your choice");
                 }
+                break;
+                case 0: {
+                    comm = "exit";
+                }
+                break;
+
             }
+
         }
     }
 
     // Method of validation inputting number
-    public static int setIntNumber() {
+    public static int getUserChoice(int choice) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(" Input number");
-        int number = 0;
-        boolean validation = false;
+        int userChoice = 0;
+        boolean validation;
         do {
-            try {
-                validation = sc.hasNextInt();
-                number = Integer.parseInt(sc.next());
-
-            } catch (InputMismatchException e) {
-                System.out.println(e);
+            validation = sc.hasNextInt();
+            if (!validation) {
+                System.out.println("Number Pls!");
                 sc.nextLine();
-            }
-        } while (!validation);
-        return number;
-    }
-
-    //Method of validation inputting operator to String[] chars1 send number of menu items
-    public static int getNumber(int number) throws NotSupportedOperationException {
-        Scanner sc = new Scanner(System.in);
-        int operator = Integer.parseInt(sc.next());
-        do {
-            if (operator <= number) {
-                break;
             } else {
-                throw new NotSupportedOperationException();
+                userChoice = sc.nextInt();
+                if (userChoice <= choice) {
+
+                } else {
+                    System.out.println("You entered incorrect number");
+                    //userChoice = sc.nextInt();
+                }
             }
-        } while (true);
-        return operator;
+        }
+        while (!validation || userChoice > choice);
+        return userChoice;
     }
 
-    static class NotSupportedOperationException extends Exception {
+    private static String getStringNotEmpty() {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        boolean validation;
+        do {
+            validation = name.isEmpty();
+            if (validation) {
+                System.out.println("String cannot be Empty.Please string!\n");
+                name = sc.nextLine();
+            } else {
 
-        public String toString() {
-            return "NotSupportedOperationException";
+                break;
+            }
+        }
+        while (validation);
+        return name;
+    }
+
+    public static void getMenu(String[] menu) {
+        List<String> listBook = Arrays.asList(menu);
+        int i = 0;
+        for (String book : listBook) {
+            if (i == 0) {
+                System.out.format("%-20s %n", book);
+            } else if (i < listBook.size()-1 ) {
+                System.out.format("%-20s [" + i + "]%n", book);
+            } else if (i <listBook.size()) {
+                System.out.format("%-20s [0]%n", book);
+            }
+            i++;
         }
     }
+
+
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        App.runPrecondition();
-        App.run();
-        
+       App.runPrecondition();
+       App.run();
     }
 }
