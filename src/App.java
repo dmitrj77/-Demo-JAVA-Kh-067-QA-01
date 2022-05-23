bb2e479d6b25bc7e45a450d1fad390310fcbe68a
Daniel Dmitry <dmitrj77@gmail.com>
2022-05-23 09:51:39 +0300

    changed title of getOperator method to getNumber and logic

--- src/App.java
+++ src/App.java
@@ -1,17 +1,20 @@
 import javafx.concurrent.Task;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
 public class App {
     static void runPrecondition() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("\nOrganizer is designed to schedule and schedule user activity. " +
+        System.out.println("\n[Organizer] is designed to schedule and schedule user activity. " +
                 "\nThe program allows you to create, edit, view, delete and restore tasks." +
                 "\nThe program has a console implementation." +
+                "\nTo control the application, " + "select a number from the menu and then follow the prompts." +
                 "\nThe program was created by the team KH-JAVA-067-QA-01." +
                 "\nThe team is the copyright holder of this program.");
         System.out.println("");
-
     }
 
     static void run() {
@@ -24,52 +27,50 @@ public class App {
         // Call method readFile from deletedTasks
         List<Task> tasks = new ArrayList<>();
         List<Task> deletedTasks = new ArrayList<>();
-        String operator;
+        int operator;
 
-        String command = "";
-        while (!command.equals("0")) {
-            System.out.println("===ORGANIZER===");
-            System.out.println("---Main menu---");
-            System.out.println("Add task    [1]");
-            System.out.println("Edit task   [2]");
-            System.out.println("Show task   [3]");
-            System.out.println("Delete task [4]");
-            System.out.println("Quit        [0]");
+        int command = 9;
+        while (command != 0) {
+            System.out.println("====ORGANIZER====");
+            System.out.println("----Main menu----");
+            System.out.println("Add task      [1]");
+            System.out.println("Edit task     [2]");
+            System.out.println("Show task     [3]");
+            System.out.println("Delete task   [4]");
+            System.out.println("Quit          [0]");
             try {
-                String[]chars=new String[]{"1","2","3","4","0"};
-                operator = getOperator(chars);
+                operator = getNumber(4);
             } catch (NotSupportedOperationException e) {
                 System.out.println("Exception:" + e);
-                operator = sc.next();
+                operator = Integer.parseInt(sc.next());
             }
             switch (operator) {
-                case "1": {
-                    String command1 = "";
-                    while (!command1.equals("0")) {
-                        System.out.println("---Add task---");
-                        System.out.println("By steps   [1]");
-                        System.out.println("By pattern [2]");
-                        System.out.println("Back       [0]");
+                case 1: {
+                    int command1 = 9;
+                    while (command1 != 0) {
+                        System.out.println("-----Add task----");
+                        System.out.println("By steps      [1]");
+                        System.out.println("By pattern    [2]");
+                        System.out.println("Back          [0]");
                         try {
-                            String[]chars=new String[]{"1","2","0"};
-                            operator = getOperator(chars);
+                            operator = getNumber(2);
                         } catch (NotSupportedOperationException e) {
                             System.out.println("Exception:" + e);
-                            operator = sc.next();
+                            operator = Integer.parseInt(sc.next());
                         }
                         switch (operator) {
-                            case "1": {
-                                System.out.println("---By steps---");
-                                System.out.println("Continue   [1]");
-                                System.out.println("Cancel     [0]");
+                            case 1: {
+                                System.out.println("-----By steps----");
+                                System.out.println("Continue      [1]");
+                                System.out.println("Cancel        [0]");
                                 try {
-                                    String[]chars=new String[]{"1","0"};
-                                    operator = getOperator(chars);
+
+                                    operator = getNumber(1);
                                 } catch (NotSupportedOperationException e) {
                                     System.out.println("Exception:" + e);
-                                    operator = sc.next();
+                                    operator = Integer.parseInt(sc.next());
                                 }
-                                if (operator.equals("1")) {
+                                if (operator == 1) {
                                     //Call method writeFile with 2 parameters " +
                                     // "(filePath and List<String> to write)
                                     System.out.println("Call method writeFile with 2 parameters " +
@@ -78,18 +79,18 @@ public class App {
                                     break;
                             }
                             break;
-                            case "2": {
-                                System.out.println("--By pattern--");
-                                System.out.println("Continue   [1]");
-                                System.out.println("Cancel     [0]");
+                            case 2: {
+                                System.out.println("----By pattern---");
+                                System.out.println("Continue      [1]");
+                                System.out.println("Cancel        [0]");
                                 try {
-                                    String[]chars=new String[]{"1","0"};
-                                    operator = getOperator(chars);
+
+                                    operator = getNumber(1);
                                 } catch (NotSupportedOperationException e) {
                                     System.out.println("Exception:" + e);
-                                    operator = sc.next();
+                                    operator = Integer.parseInt(sc.next());
                                 }
-                                if (operator.equals("1")) {
+                                if (operator == 1) {
                                     //Call method writeFile with 2 parameters " +
                                     // "(filePath and List<String> to write)
                                     System.out.println("Call method writeFile with 2 parameters " +
@@ -98,30 +99,30 @@ public class App {
                                     break;
                             }
                             break;
-                            case "0": {
-                                command1 = "0";
+                            case 0: {
+                                command1 = 0;
                                 break;
                             }
                         }
                     }
                 }
                 break;
-                case "2": {
-                    String command2 = "";
-                    while (!command2.equals("0")) {
-                        System.out.println("---Edit task---");
-                        System.out.println("Existing    [1]");
-                        System.out.println("Recycle Bin [2]");
-                        System.out.println("Back        [0]");
+                case 2: {
+                    int command2 = 9;
+                    while (command2 != 0) {
+                        System.out.println("-----Edit task---");
+                        System.out.println("Existing      [1]");
+                        System.out.println("Recycle Bin   [2]");
+                        System.out.println("Back          [0]");
                         try {
-                            String[]chars=new String[]{"1","2","0"};
-                            operator = getOperator(chars);
+                            // String[] chars = new String[]{"1", "2", "0"};
+                            operator = getNumber(2);
                         } catch (NotSupportedOperationException e) {
                             System.out.println("Exception:" + e);
-                            operator = sc.next();
+                            operator = Integer.parseInt(sc.next());
                         }
                         switch (operator) {
-                            case "1": {
+                            case 1: {
                                 System.out.println("--Existing--");
                                 //Call method readFile from List<Task> tasks = new ArrayList<>();
                                 System.out.println("Call method readFile from List<Task> " +
@@ -130,7 +131,7 @@ public class App {
                                         "(filePath and List<String> to write)  ");
                                 break;
                             }
-                            case "2": {
+                            case 2: {
                                 System.out.println("-Recycle Bin-");
                                 //Call method readFile from List<Task> deletedTasks = new ArrayList<>();
                                 System.out.println("Call method readFile from List<Task> " +
@@ -139,73 +140,71 @@ public class App {
                                         "(filePath and List<String> to write)  ");
                                 break;
                             }
-                            case "0": {
-                                command2 = "0";
+                            case 0: {
+                                command2 = 0;
                                 break;
                             }
                         }
                     }
                 }
                 break;
-                case "3": {
-                    String command3 = "";
-                    while (!command3.equals("0")) {
-                        System.out.println("----Showing----");
-                        System.out.println("All tasks   [1]");
-                        System.out.println("By filter   [2]");
-                        System.out.println("Deleted     [3]");
-                        System.out.println("Back        [0]");
+                case 3: {
+                    int command3 = 9;
+                    while (command3 != 0) {
+                        System.out.println("-----Showing-----");
+                        System.out.println("All tasks     [1]");
+                        System.out.println("By filter     [2]");
+                        System.out.println("Deleted       [3]");
+                        System.out.println("Back          [0]");
                         try {
-                            String[]chars=new String[]{"1","2","3","0"};
-                            operator = getOperator(chars);
+                            operator = getNumber(3);
                         } catch (NotSupportedOperationException e) {
                             System.out.println("Exception:" + e);
-                            operator = sc.next();
+                            operator = Integer.parseInt(sc.next());
                         }
                         switch (operator) {
-                            case "1": {
+                            case 1: {
                                 System.out.println("--All tasks--");
                                 System.out.println("Call method readFile from List<Task> " +
                                         "tasks = new ArrayList<>()");
                                 break;
                             }
-                            case "2": {
+                            case 2: {
                                 System.out.println("--By filter--");
                                 System.out.println("Call method readFileByFilter from List<Task> " +
                                         "tasks = new ArrayList<>()");
                                 break;
                             }
-                            case "3": {
+                            case 3: {
                                 System.out.println("---Deleted---");
                                 //Call method readFile from List<Task> deletedTasks = new ArrayList<>();
                                 System.out.println("Call method readFile from List<Task> " +
                                         "deletedTasks = new ArrayList<>();");
                                 break;
                             }
-                            case "0": {
-                                command3 = "0";
+                            case 0: {
+                                command3 = 0;
                                 break;
                             }
                         }
                     }
                 }
                 break;
-                case "4": {
-                    String command4 = "";
-                    while (!command4.equals("0")) {
-                        System.out.println("----Delete----");
-                        System.out.println("By Id      [1]");
-                        System.out.println("By period  [2]");
-                        System.out.println("Back       [0]");
+                case 4: {
+                    int command4 = 9;
+                    while (command4 != 0) {
+                        System.out.println("------Delete-----");
+                        System.out.println("By Id         [1]");
+                        System.out.println("By period     [2]");
+                        System.out.println("Back          [0]");
                         try {
-                            String[]chars=new String[]{"1","2","0"};
-                            operator = getOperator(chars);
+                            operator = getNumber(2);
                         } catch (NotSupportedOperationException e) {
                             System.out.println("Exception:" + e);
-                            operator = sc.next();
+                            operator = Integer.parseInt(sc.next());
                         }
                         switch (operator) {
-                            case "1": {
+                            case 1: {
                                 System.out.println("----By Id----");
                                 System.out.println("Call method readFile from List<Task> " +
                                         "tasks = new ArrayList<>()");
@@ -213,7 +212,7 @@ public class App {
                                         "tasks = new ArrayList<>()");
                                 break;
                             }
-                            case "2": {
+                            case 2: {
                                 System.out.println("--By period--");
                                 System.out.println("Call method readFile from List<Task> " +
                                         "tasks = new ArrayList<>()");
@@ -221,16 +220,16 @@ public class App {
                                         "tasks = new ArrayList<>()");
                                 break;
                             }
-                            case "0": {
-                                command4 = "0";
+                            case 0: {
+                                command4 = 0;
                                 break;
                             }
                         }
                     }
                 }
                 break;
-                case "0": {
-                    command = "0";
+                case 0: {
+                    command = 0;
                     break;
                 }
             }
@@ -246,7 +245,7 @@ public class App {
         do {
             try {
                 validation = sc.hasNextInt();
-                number = sc.nextInt();
+                number = Integer.parseInt(sc.next());
 
             } catch (InputMismatchException e) {
                 System.out.println(e);
@@ -257,19 +256,16 @@ public class App {
     }
 
     //Method of validation inputting operator to String[] chars1 send number of menu items
-    public static String getOperator(String[] chars1) throws NotSupportedOperationException {
+    public static int getNumber(int number) throws NotSupportedOperationException {
         Scanner sc = new Scanner(System.in);
-        String operator = sc.next();
-        List<String> chars = Arrays.asList(chars1);
-        boolean validation;
-
+        int operator = Integer.parseInt(sc.next());
         do {
-            if (validation = chars.contains(operator)) {
+            if (operator <= number) {
                 break;
             } else {
                 throw new NotSupportedOperationException();
             }
-        } while (!validation);
+        } while (true);
         return operator;
     }
 
