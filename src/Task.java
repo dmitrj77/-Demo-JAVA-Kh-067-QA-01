6536b4378e0d6b440ad8e7aee69353c67ab277ea
SinenkoA <sinenko.alexey66@gmail.com>
2022-05-25 13:15:31 +0300

    Added package utils, implemented method getLocalDateTime, changed toString and deleted taskList

--- src/Task.java
+++ src/Task.java
@@ -1,14 +1,31 @@
+import utils.DateUtils;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 
-public class Task {
+public class Task extends DateUtils {
     private String title;
     private LocalDateTime localDateTime;
     private String description;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return title;
+    }
+
+    public void setDescription(String description) {
+        this.title = title;
+    }
 
     // constrictor
     public Task(String title, LocalDateTime localDateTime, String description) {
@@ -21,42 +38,13 @@ public class Task {
     public Task(String string) {
         String[] taskFields = string.split(";");
         this.title = taskFields[0];
-        // использовать метод для получения LocalDateTime из DateUtils.java
-        this.localDateTime = null;
+        this.localDateTime = getLocalDateTime(taskFields[1]);// метод не хочет работать не пойму в чем проблема
         this.description = taskFields[2];
     }
 
     @Override
     public String toString() {
-        return " " + localDateTime + " " + title + description;
+        return "Date:" + localDateTime + ". Title:" + title + ". Description:" + description + ".";
     }
-
-
     //метод для записи Таска в Файл уже написан в FileUtils (writeFile) принимает Лист и его прописывает а файл
-    //метод преобразования из String в List
-    public static List<String> taskList(String string) {
-        List<String> list = new ArrayList<>();
-        try {
-            list = Arrays.asList(string.split("\\s*;\\s*"));
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return list;
-    }
-
-    //мейн черновик для проверки
-    public static void main(String[] args) {
-        String string = "12.12.12;Title;Description";
-        String[] strings = string.split(";");
-        System.out.println(strings[0]);
-        System.out.println(strings[1]);
-        System.out.println(strings[2]);
-
-        Task task = new Task(string);
-        System.out.println(task);
-
-        System.out.println(taskList("12.12.12;Title;Description"));
-
-    }
-
 }
